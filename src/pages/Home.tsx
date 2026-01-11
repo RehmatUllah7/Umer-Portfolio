450cb131d189ddd32902045bd05d607494332c6f
matharehsan <muhammadatharehsan2@gamil.com>
2026-01-12 02:11:03 +0500

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import Feature from "../components/feature";
+
+const Home = () => {
+  return (
+    <>
+    
+      <Hero />
+      <Feature />
+      <Projects />
+      
+      
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
